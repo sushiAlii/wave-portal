7a2c02b6e3e87553aeb17c3fb5d0cb45d526d3a0
sushiAlii <aliasgar.laut@gmail.com>
2021-10-24 15:57:07 +0800

    Adding deploy.js to deploy on the testnet

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const hre = require("hardhat");
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.001"),
+  });
+
+  await waveContract.deployed();
+
+  // const [deployer] = await hre.ethers.getSigners();
+  // const accountBalance = await deployer.getBalance();
+
+  // console.log("Account Deployed by", deployer.address);
+  // console.log("Balance:", accountBalance.toString());
+  console.log("Wave Portal Address:", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log("Something Happened!", error);
+    process.exit(1);
+  }
+};
+
+runMain();
