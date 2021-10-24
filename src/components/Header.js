98aafa0410962ced9fd4920032e3b421d02d5040
sushiAlii <aliasgar.laut@gmail.com>
2021-10-25 00:51:08 +0800

    Finalizing Headers Component

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { Text, Button, Input, Flex, Center } from "@chakra-ui/react";
+import { FaEthereum } from "react-icons/fa";
+import { WavesList } from "./WavesList";
+export const Header = ({ waves, message, setMessage, waveExecute }) => {
+  const handleInput = (event) => {
+    setMessage(event.target.value);
+    console.log(message);
+  };
+
+  return (
+    <Center mt={10}>
+      <header>
+        <div>
+          <Text fontSize="5xl">👋 Sushi's Wave Portal! 🍣</Text>
+          <Text fontSize="2xl">Try and Wave me! Using Rinkeby Testnet</Text>
+        </div>
+        <div>
+          <Flex mx="auto" mt={12} maxW="2xl">
+            <Input
+              placeholder="Drop a Message!"
+              size="md"
+              value={message}
+              onChange={handleInput}
+            ></Input>
+          </Flex>
+        </div>
+        <div className="button">
+          <Button onClick={waveExecute} leftIcon={<FaEthereum />}>
+            Wave
+          </Button>
+        </div>
+        {waves.map((wave, index) => {
+          return (
+            <Center>
+              <WavesList wave={wave} index={index} />{" "}
+            </Center>
+          );
+        })}
+      </header>
+    </Center>
+  );
+};
