ec4a007569e5cc371589636c766602f0dcde0523
sushiAlii <aliasgar.laut@gmail.com>
2021-10-24 15:56:35 +0800

    Added wave function

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,7 +8,7 @@ contract WavePortal {
     uint256 totalWaves;
     uint256 private seed;
 
-    event newWave(address indexed from, uint256 timestamp, string message);
+    event NewWave(address indexed from, uint256 timestamp, string message);
 
     struct Wave {
         address waver;
@@ -19,11 +19,43 @@ contract WavePortal {
 
     mapping(address => uint256) public lastWavedAt;
 
-    constructor() {
+    constructor() payable {
         console.log("Welcome to Wave Contract!");
     }
 
-    function wave(string memory _message) public {}
+    function wave(string memory _message) public {
+        // require(
+        //     lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+        //     "30 Seconds Interval"
+        // );
+
+        // lastWavedAt[msg.sender] = block.timestamp;
+
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        uint256 randomNumber = (block.difficulty + block.timestamp + seed) %
+            100;
+        console.log("Generated Random Number", randomNumber);
+
+        seed = randomNumber;
+
+        if (randomNumber < 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
 
     function getAllWaves() public view returns (Wave[] memory) {
         return waves;
