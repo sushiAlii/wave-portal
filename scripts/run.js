610143642c8851cc492c88df2299e7402ebf6ade
sushiAlii <aliasgar.laut@gmail.com>
2021-10-24 15:56:49 +0800

    Added run.js for deploying locally

---
+++ scripts/run.js
@@ -0,0 +1,49 @@
+const hre = require("hardhat");
+
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+  console.log("Contract was Deployed Successfully!", waveContract.address);
+
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract Remaining Balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log("People who waved:", waveCount.toNumber());
+
+  const waveTxn = await waveContract.wave("Message 1 Test");
+  await waveTxn.wait();
+
+  const waveTxn2 = await waveContract.wave("Message 2 Test");
+  await waveTxn2.wait();
+
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract Remaining Balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log("Something Happened!", error);
+    process.exit(1);
+  }
+};
+
+runMain();
